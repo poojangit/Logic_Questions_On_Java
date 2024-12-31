35514c2ffd8ed7e5efbbd3ebbd25384aac13ac90
poojangit <poojang147@gmail.com>
2024-12-31 10:09:53 +0530

    [POOJA] Added: Program to reverse the string and check for palindrome

---
+++ strings/PalindromeCheck.java
@@ -0,0 +1,18 @@
+package strings;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string to check the palindrame");
+        String str = sc.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+        if(str.equals(reversed)){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+        sc.close();
+    }
+}
