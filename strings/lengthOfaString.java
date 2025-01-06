aac88556500335a0e652bcca9a97d192667c62e3
poojangit <poojang147@gmail.com>
2025-01-06 17:22:20 +0530

    [POOJA] Added : to find the length of string without using the length

---
+++ strings/lengthOfaString.java
@@ -0,0 +1,17 @@
+package strings;
+
+import java.util.Scanner;
+
+public class lengthOfaString {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a String");
+    String str = sc.nextLine();
+    int length = 0;
+    for(char c : str.toCharArray()){
+        length++;
+    }
+    System.out.println("Length is " + length);
+    sc.close();
+}
+}
